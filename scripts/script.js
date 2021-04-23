2de57717433040ee03b2c9a798e6c56a919ec24f
Gabriel Yago Moreira <gymoreira@outlook.com>
2021-04-23 19:52:37 -0300

    add Consumo feature

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,23 @@
+var horas = new Array();
+var mes = [0,0,0,0,0,0,0,0,0,0,0,0];
+var months = ["JANEIRO","FEVEREIRO","MARÇO","ABRIL","MAIO","JUNHO","JULHO","AGOSTO","SETEMBRO","OUTUBRO","NOVEMBRO ","DEZEMBRO"];
 
 
 const Status = {
+    //Liga a lampada e começa a contar o consumo
     on() {
+        ligarConsumo()
         console.log('Ligar')
         document.getElementById('lamp').style.backgroundColor= 'yellow'
+        
     },
+    //Desliga a lampada e atualiza o consumo
     off() {
+        
         console.log('Desligar')
         document.getElementById('lamp').style.backgroundColor= 'gray'
+        limpaTable();
+        desligarConsumo();
 
     }
 }
@@ -31,3 +41,57 @@ const  day_week = 'segunda-feira'
 const data = '23'
 const hour = '10'
 const minute = '50'
+
+
+//ligarConsumo()//deixar essa linha comentada se o sistema vai assumir que ele inicia com a lampada apagada.
+/* Pega o horario que o user ligou a lampada e o armazena */
+function ligarConsumo() {
+    var datanow = new Date();
+    // botao Ligar
+    var horario = Date.parse(datanow);// alterar datanow OU "April 24, 2021"
+    if (horas.length==0){
+        //botao Desligar
+        horas.push(horario);
+    }    
+}
+/* Pega o horario que o user desligou a lampada e atuliza o consumo mensal */
+function desligarConsumo(){
+    var datanow = new Date();
+    // botao Ligar
+    var horario = Date.parse(datanow);// alterar datanow OU "April 24, 2021"
+    var mesNumero = datanow.getMonth();
+    //var test = Date.parse("April 24, 2021");// test
+    //horas.push(test); //test
+    if (horas.length==1){
+        //botao Desligar
+        horas.push(horario);
+        var tempogasto = (horas[1]-horas[0])/3600000;
+        mes[mesNumero]+=tempogasto;
+        horas.shift();
+        horas.shift();
+    }
+    gerarTable();
+}
+  
+gerarTable();
+//gera a tabela com o consumo atualizado
+function gerarTable() {
+    for (i = 0; i < 12; i++) {
+    var tabela = document.getElementById('consumo');
+    var numeroLinhas = tabela.rows.length;
+    var linha = tabela.insertRow(numeroLinhas);
+    var celula1 = linha.insertCell(0);
+    var celula2 = linha.insertCell(1);   
+    var celula3 = linha.insertCell(2);
+    
+    celula1.innerHTML = months[i]; 
+    celula2.innerHTML =  mes[i].toFixed(2); 
+    celula3.innerHTML =  (mes[i] * 0.82).toFixed(6);
+}
+}
+// Apaga a tabela com as informações antigas
+function limpaTable() {
+    for (i = 0; i < 12; i++) {
+    document.getElementById('consumo').deleteRow(1);
+    }
+  }  
