87db74e5b9f500cc513fafe8a2c96a1616a3b908
Gabriel Yago Moreira <gymoreira@outlook.com>
2021-05-04 01:22:56 -0300

    v2.0

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ const app = express();
 const bodyParser = require('body-parser'); // Usado para obter os dados da hora e dia da semana no HTML
 const fs = require('fs');
 
-let timer
-let resultTimer = 0
+let horas;
+let timer = 0
+let comandLamp
 let counting
-let data; //Variável para ler o estado do arquivo (Somente a definição)
+vectorTopic = ['state', 'alive', 'stateLamp'] //Vetor com os tópicos inscritos
+let data; //Variável para ler o estado da lâmpada dentro do arquivo 
 let stateNode = 'Carregando...'
 let count = 0
-let dataWeek
-let horas;
-let tempos;
-
 
 // Template enginer como é chamado.
 app.set('view engine', 'ejs')
@@ -27,8 +25,6 @@ app.use('/', express.static('/'))
 app.use('/styles', express.static(__dirname + '/styles'))
 app.use('/scripts', express.static(__dirname + '/scripts'))
 
-
-
 var device = awsIot.jobs({
   keyPath: 'tmp/01e7c0c317-private.pem.key',
   certPath: 'tmp/01e7c0c317-certificate.pem.crt',
@@ -37,106 +33,173 @@ var device = awsIot.jobs({
   host: 'a1d0uhf3egac5n-ats.iot.us-east-1.amazonaws.com'
 });
 
+const operationJSON = {
+  write(json, data) {
+    fs.writeFileSync(`./data/${json}.json`, JSON.stringify(data), 'utf-8')
+  },
+  read(json) {
+    return JSON.parse(fs.readFileSync(`./data/${json}.json`, 'utf-8'))
+  },
+  delete(positon) {
+    let data = this.read('store_Program');
+    data.splice(positon, 1);
+    this.write('store_Program', data);
+  }
+}
 
+let statebutton = 0;
+const publish = {
+  onLamp() {
+    if (statebutton == 0) {
+    device.publish('lamp', '1');
+      calculateTime.start();
+      statebutton = 1;
+    }
+  },
+
+  offLamp() {
+    if (statebutton == 1) {
+      device.publish('lamp', '0');
+      calculateTime.end();
+      statebutton = 0;
+    }
+  }
+}
+
+const calculateTime = {
+  start() {
+    let tempos = operationJSON.read('tempos'); //Ler o arquivo e retorna no formato JSON
+    var datanow = convertUTC();
+    var horario = Date.parse(datanow); // alterar datanow OU "April 24, 2021"
+    if (tempos[0].tempo == 0) {
+      tempos[0].tempo = horario
+    }
+    operationJSON.write('tempos', tempos); //Escreve no arquivo com o novo objeto adicionado
+  },
+  end() {
+    let tempos = operationJSON.read('tempos') //Ler o arquivo e retorna no formato JSON
+    if (tempos[0].tempo != 0) {
+      let horas = operationJSON.read('horas') //Ler o arquivo e retorna no formato JSON
+      var datanow = convertUTC();
+      var horario = Date.parse(datanow); // alterar datanow OU "April 24, 2021"
+      var mesNumero = datanow.getMonth();
+      var tempogasto = horas.hora[mesNumero]
+      tempogasto += ((horario - tempos[0].tempo) / 3600000);
+      horas.hora[mesNumero] = tempogasto
+      operationJSON.write('horas', horas) //Escreve no arquivo com o novo objeto adicionado
+      tempos[0].tempo = 0
+      operationJSON.write('tempos', tempos) //Escreve no arquivo com o novo objeto adicionado
+
+    }
+  }
+}
 
 /*******************************************************************************
  * Obtem os dados da semana e hora e armazena no arquivo store_Program
  */
-
 app.post('/', (req, res) => {
-  dataWeek = JSON.parse(fs.readFileSync('./data/store_Program.json', 'utf-8')) //Ler o arquivo e retorna no formato JSON
-  dataWeek.push({
-    "semana": req.body.week,
-    "horario": req.body.hour
-  }) // Adiciona na variável os conteúdos
-  fs.writeFileSync('./data/store_Program.json', JSON.stringify(dataWeek), 'utf-8') //Escreve no arquivo com o novo objeto adicionado
+  let day
+  let getData = {
+    "week": req.body.week,
+    "hour": req.body.hour,
+    "action": req.body.selectState
+  }
+  day = operationJSON.read('store_Program') //Ler o arquivo e retorna no formato JSON
+  day.push(getData)
+  operationJSON.write('store_Program', day) //Escreve no arquivo com o novo objeto adicionado 
+  copyJSONProgram();
   return res.redirect('/')
 })
-/*****************************************************************************/
 
 // Através do método post usamos para poder enviar um publicação
 // Aqui é enviado a publicação para a lâmpada ligar
-/* Pega o horario que o user ligou a lampada e o armazena */
-
 app.post('/on', (req, res) => {
-  tempos = JSON.parse(fs.readFileSync('./data/tempos.json', 'utf-8')) //Ler o arquivo e retorna no formato JSON
-  var datanow = new Date();
-    var horario = Date.parse(datanow);// alterar datanow OU "April 24, 2021"
-    if (tempos[0].tempo==0){
-      tempos[0].tempo=horario
-    }
-  fs.writeFileSync('./data/tempos.json', JSON.stringify(tempos), 'utf-8') //Escreve no arquivo com o novo objeto adicionado
   console.log("on")
-  OnOffLamp('l')
+  publish.onLamp()
   return res.redirect('/') //Após o click no botão de ligar a página é redirecionada própria página principal
 
 })
 
 //Através do método post usamos para poder enviar um publicação 
 // Aqui é enviado a publicação para a lâmpada desligar
-/* Pega o horario que o user desligou a lampada e atuliza o consumo mensal */
 app.post('/off', (req, res) => {
-  if (tempos[0].tempo!=0){
-    horas = JSON.parse(fs.readFileSync('./data/horas.json', 'utf-8')) //Ler o arquivo e retorna no formato JSON
-    tempos = JSON.parse(fs.readFileSync('./data/tempos.json', 'utf-8')) //Ler o arquivo e retorna no formato JSON
-    var datanow = new Date();
-    var horario = Date.parse(datanow);// alterar datanow OU "April 24, 2021"
-    var mesNumero = datanow.getMonth();
-    var tempogasto = horas.hora[mesNumero]
-    tempogasto += ((horario-tempos[0].tempo)/3600000);
-    horas.hora[mesNumero] = tempogasto
-    fs.writeFileSync('./data/horas.json', JSON.stringify(horas), 'utf-8') //Escreve no arquivo com o novo objeto adicionado
-    tempos[0].tempo=0
-    fs.writeFileSync('./data/tempos.json', JSON.stringify(tempos), 'utf-8') //Escreve no arquivo com o novo objeto adicionado
-  }
   console.log("off")
-  OnOffLamp('d')
+  publish.offLamp()
   return res.redirect('/')
 })
 
+app.post('/delete', (req, res) => {
+  operationJSON.delete(parseInt(req.body.positionDelete))
+  copyJSONProgram() // Rescrever no arquivo os valores alterando-os para mostrar na tela
+  return res.redirect('/');
+})
+function runProgram() {
+  let time = 1000;
+  let date = new Date();
+  let brweek
+  let brhour
+  if (date.getUTCHours()<3){
+    brweek = date.getUTCDay() - 1;
+    if(date.getUTCHours()==2){
+      brhour=23
+    }
+    if(date.getUTCHours()==1){
+      brhour=22
+    }
+    if(date.getUTCHours()==0){
+      brhour=21
+    }
+  } else {
+    brhour=date.getUTCHours()-3
+    brweek = date.getUTCDay()
+  }
+  let temp = operationJSON.read('store_Program');
+  if (temp[0] == null) {
+  } else {
+    for (i in temp) {
+      let array = temp[i].hour.split(':'); //Retirar os ':' da hora e retora um array com as palavra separadas em cada posição
+      if (brweek.toString() === temp[i].week) {
+        console.log("oxe, porra")
+        console.log(brweek)
+        if (brhour == parseInt(array[0]) && date.getMinutes() == parseInt(array[1])) {
+          console.log("ta louco nao pq?")
+          time = 60000
+          temp[i].action === '1' ? publish.onLamp() : publish.offLamp();
+        } else time = 1000;
+      }
+    }
+  }
+  repeat = setTimeout(runProgram, time);
+}
 
 /**************************************************************************************************************
  * Através desse metódo post podemos obter os valores dos inputs com o tempo para setar o temporizador
  * 
  */
-
 app.post('/timer', (req, res) => {
-  
-  timer = (parseInt(req.body.timer) * 60) + (parseInt(req.body.timer[3] + req.body.timer[4]))
-  countTime(req.body.timerSelect[0]) // Obtem a primeira letra da palavra l = ligar , d = desliga
-  return res.redirect('/')
+  timer = (parseInt(req.body.timer) * 60 * 60) + (parseInt(req.body.timer[3] + req.body.timer[4]))
+  if (timer>0){   
+    comandLamp = req.body.timerSelect[0]
+    setTimeout(() => {
+      countTime() // Obtem a primeira letra da palavra l = ligar , d = desliga
+    }, 1000)
+    return res.redirect('/')
+  }
 })
 
-function OnOffLamp(comand){
-  if(comand === 'l'){
-    device.publish('lamp', '1')  
-  }else{
-    device.publish('lamp', '0')
+//Função que decrementa a váriavel do temporizador
+function countTime() {  
+  timer -= 1
+  counting = setTimeout(countTime, 1000);
+  if (timer == 0) {
+    if (comandLamp === 'l') {
+      publish.onLamp()
+    } else publish.offLamp()
+    clearTimeout(counting)
   }
 }
 
-//Função que decrementa a váriavel em segundos
-
-function countTime(value) {
-     counting = setInterval(() => {
-    timer -= 1
-    resultTimer = timer // A cada decremtento é atribuido para resultTimer para poder mostrar na tela o decremento
-    console.log(timer)
-    if (timer == 0) {
-      OnOffLamp(value) // Chama a função para ligar ou desligar dependendo da mensagem
-      stopTimer()     // Para a contagem. Nesse caso a contagem do temporizador
-    }
-  }, 1000)
-}
-
-
-function stopTimer(){
-  clearInterval(counting)
-}
 /************************************************************************************************************** */
-
-//Vetor com os tópicos inscritos
-vectorTopic = ['state', 'alive', 'stateLamp']
 //Conecta com o AWS IOT e se inscreve nos tópicos
 device
   .on('connect', function () {
@@ -148,80 +211,104 @@ device
 
 //Função que recebe a publicação sempre que houver uma publicação
 device
-  .on('message', function (topic, payload) {
-    //console.log('Tópico', topic, payload.toString());
+.on('message', function (topic, payload) {
 
-    if (topic === 'state_esp') {
-      stateLamp = payload.toString()
-    }
-
-    //Verificar se o nodemcu está conectado ao aws recebendo publicações que avisa que está conectado
-    if (topic === 'alive') {
-      stateNode = 'Online' //variável usada para mostrar o estado do node em tela
-      count = 0; // Zerar o contado para que possa mater a informação de que o Node MCU está conectado
-    }
-
-    if (topic === 'stateLamp') {
-      
-      const update = JSON.stringify({
-        state: payload.toString()
-      })
-      fs.writeFileSync('./data/state.json', update, 'utf-8') //Escrever o estado da lâmpada recebida na publicação
-    }
-  });
-
-
-//Contagem para identificar se o Node MCU está conectado ou desconectado.
-/**
- * Quando a decorrido o tempo de no máximo 15 segundo se não houver publicção do Node MCU é atribuida à variável 
- * o valor de offline que é mostrado na interface
- */
-setInterval(function () {
-  count += 1
-  if (count >= 9) {
-    count = 0;
-    stateNode = 'Offline'
+  //Verificar se o nodemcu está conectado ao aws recebendo publicações que avisa que está conectado
+  if (topic === 'alive') {
+    count = 0; // Zerar o contado para que possa mater a informação de que o Node MCU está conectado
+    stateNode = 'Online' //variável usada para mostrar o estado do node em tela
   }
-}, 1000)
+  if (topic === 'stateLamp') {
+    let getStateLamp = payload.toString();
+    if (getStateLamp === 'l') {
+      calculateTime.start();
+      statebutton = 1;
+    } else {
+      calculateTime.end();
+      statebutton = 0;
+    }
+    const update = {
+      state: getStateLamp
+    }
+    operationJSON.write('state', update); //Escrever o estado da lâmpada recebida na publicação
+  }
+});
 
+/**
+*Contagem para identificar se o Node MCU está conectado ou desconectado.
+* Quando a decorrido o tempo de no máximo 15 segundo se não houver publicção do Node MCU é atribuida à variável 
+* o valor de offline que é mostrado na interface
+*/
+setInterval(function () {
+count += 1
+if (count > 40) {
+  count = 0;
+  stateNode = 'Offline';
+}
+}, 1000)
+setTimeout(() => {
+//colocar a funcao de verificar se é o dia posterior ou nao para poder executar só uma vez
+}, 10)
+
+function copyJSONProgram() {
+let read = operationJSON.read('store_Program');
+let dayWeek = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+for (i in read) {
+  read[i].week = dayWeek[read[i].week];
+
+  read[i].action === '1' ? read[i].action = 'Ligar' : read[i].action = 'Desligar';
+
+}
+operationJSON.write('copy_Program', read);
+}
+
+function convertUTC() {
+return new Date(new Date().getTime() - 180 * 60 * 1000)
+}
 
 /**
 * Essas variáveis estão sendo sempre alteradas. No text é mostrado o estado do Node MCU, text2 o estado da lâmpada
 * e no text3 é resultado do temporizado que está decrementando 
 */
-
 app.get('/', (req, res) => {
-  horas = JSON.parse(fs.readFileSync('./data/horas.json', 'utf-8')) //Ler o arquivo e retorna no formato JSON
-  data = JSON.parse(fs.readFileSync('./data/state.json', 'utf-8')) // Lendo arquivo JSON com o estado da lâmpada
-  res.render("../views/index", {
-    text: stateNode,
-    text2: data.state,
-    texto3: resultTimer,
-    hr0: (horas.hora[0] * 10).toFixed(2),
-    vl0: (horas.hora[0] * 0.003).toFixed(6),
-    hr1: (horas.hora[1] * 10).toFixed(2),
-    vl1: (horas.hora[1] * 0.003).toFixed(6),
-    hr2: (horas.hora[2] * 10).toFixed(2),
-    vl2: (horas.hora[2] * 0.003).toFixed(6),
-    hr3: (horas.hora[3] * 10).toFixed(2),
-    vl3: (horas.hora[3] * 0.003).toFixed(6),
-    hr4: (horas.hora[4] * 10).toFixed(2),
-    vl4: (horas.hora[4] * 0.003).toFixed(6),
-    hr5: (horas.hora[5] * 10).toFixed(2),
-    vl5: (horas.hora[5] * 0.003).toFixed(6),
-    hr6: (horas.hora[6] * 10).toFixed(2),
-    vl6: (horas.hora[6] * 0.003).toFixed(6),
-    hr7: (horas.hora[7] * 10).toFixed(2),
-    vl7: (horas.hora[7] * 0.003).toFixed(6),
-    hr8: (horas.hora[8] * 10).toFixed(2),
-    vl8: (horas.hora[8] * 0.003).toFixed(6),
-    hr9: (horas.hora[9] * 10).toFixed(2),
-    vl9: (horas.hora[9] * 0.003).toFixed(6),
-    hr10: (horas.hora[10] * 10).toFixed(2),
-    vl10: (horas.hora[10] * 0.003).toFixed(6),
-    hr11: (horas.hora[11] * 10).toFixed(2),
-    vl11: (horas.hora[11] * 0.003).toFixed(6)
-  })
+data = operationJSON.read('state'); // Ler o arquivo JSON com o estado da lâmpada
+horas = operationJSON.read('horas'); //Ler o arquivo e retorna no formato JSON
+let dataCopy = operationJSON.read('copy_Program');
+res.render("../views/index", {
+  showData: dataCopy,
+  text: stateNode,
+  text2: data.state,
+  texto3: timer,
+  hr0: (horas.hora[0] * 10).toFixed(2),
+  vl0: (horas.hora[0] * 0.003).toFixed(6),
+  hr1: (horas.hora[1] * 10).toFixed(2),
+  vl1: (horas.hora[1] * 0.003).toFixed(6),
+  hr2: (horas.hora[2] * 10).toFixed(2),
+  vl2: (horas.hora[2] * 0.003).toFixed(6),
+  hr3: (horas.hora[3] * 10).toFixed(2),
+  vl3: (horas.hora[3] * 0.003).toFixed(6),
+  hr4: (horas.hora[4] * 10).toFixed(2),
+  vl4: (horas.hora[4] * 0.003).toFixed(6),
+  hr5: (horas.hora[5] * 10).toFixed(2),
+  vl5: (horas.hora[5] * 0.003).toFixed(6),
+  hr6: (horas.hora[6] * 10).toFixed(2),
+  vl6: (horas.hora[6] * 0.003).toFixed(6),
+  hr7: (horas.hora[7] * 10).toFixed(2),
+  vl7: (horas.hora[7] * 0.003).toFixed(6),
+  hr8: (horas.hora[8] * 10).toFixed(2),
+  vl8: (horas.hora[8] * 0.003).toFixed(6),
+  hr9: (horas.hora[9] * 10).toFixed(2),
+  vl9: (horas.hora[9] * 0.003).toFixed(6),
+  hr10: (horas.hora[10] * 10).toFixed(2),
+  vl10: (horas.hora[10] * 0.003).toFixed(6),
+  hr11: (horas.hora[11] * 10).toFixed(2),
+  vl11: (horas.hora[11] * 0.003).toFixed(6)
 })
+})
+
+/***************************** Chamada das funções *************************/
+runProgram();
+copyJSONProgram();
+/**************************************************************************/
 
 app.listen(process.env.PORT || 3000); //Definido a porta padrão do servidor
--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,34 @@
+let loop;
+
 function initalState() {
     const stateESP = document.getElementById('stateESP').innerText
     if (stateESP !== 'Online') {
-        document.getElementById('on').disabled = true
-        document.getElementById('off').disabled = true
+        document.getElementById('on').disabled = true;
+        document.getElementById('off').disabled = true;
+        document.getElementById('initTimer').disabled = true;
     } else {
-        document.getElementById('on').disabled = false
-        document.getElementById('off').disabled = false
+        document.getElementById('on').disabled = false;
+        document.getElementById('off').disabled = false;
+        document.getElementById('initTimer').disabled = false;
+
     }
 }
 
-setInterval(()=>{
+function updateValues() {
+
+    let date = new Date(new Date().getTime() - 180 * 60 * 1000);
     const stateLamp = document.getElementById('stateLamp').innerText
     const disabledButtonTimer = document.getElementById('timerResult').innerText
-    
+
+    const colorLamp = document.getElementById('lamp')
+    const baseLamp = document.getElementById('lamp1')
+
+    // Atualiza os valores do servido na página
+    $('#stateESP').load(window.location.href + " " + '#stateESP');
+    $('#stateLamp').load(window.location.href + " " + '#stateLamp');
+    $('#timerResult').load(window.location.href + " " + '#timerResult')
+    $(`#mes${date.getMonth()}`).load(window.location.href + " " + `#mes${date.getMonth()}`)
+    $(`#mes${date.getMonth()}value`).load(window.location.href + " " + `#mes${date.getMonth()}value`)
 
     if (disabledButtonTimer === '0') {
         document.getElementById('initTimer').disabled = false
@@ -20,25 +36,18 @@ setInterval(()=>{
 
 
     if (stateLamp === 'l') {
-        document.getElementById('lamp').style.backgroundColor = 'yellow'
+        colorLamp.style.backgroundColor = '#EBCE2A'
+        colorLamp.style.boxShadow = " inset 1px 1px 20px #E8E1AB, 0 0 5rem #FAF378, 0 0 50px black"
+        baseLamp.style.backgroundColor = '#EBCE2A'
 
     } else if (stateLamp === 'd') {
-        document.getElementById('lamp').style.backgroundColor = 'rgb(59, 59, 2)'
-
-    } else {
-        document.getElementById('lamp').style.backgroundColor = 'gray'
+        colorLamp.style.backgroundColor = 'gray'
+        baseLamp.style.backgroundColor = 'gray'
+        colorLamp.style.boxShadow = "1px 1px 50px black"
     }
     initalState() //Desbilitar ou habilita os botões quando desconectado ou iniciando a conexão
-
-    // Atualiza os valores do servido na página
-    $('#stateESP').load(window.location.href + " " + '#stateESP');
-    $('#stateLamp').load(window.location.href + " " + '#stateLamp');
-    $('#timerResult').load(window.location.href + " " + '#timerResult')
-    
-},100)
-
-
-
+    loop = setTimeout(updateValues, 50)
+}
 
 // Mostrar ou retirar da tela a parte de programação ao clicar no botão
 const Schedule = {
@@ -49,3 +58,6 @@ const Schedule = {
         document.querySelector('.schedule-week').classList.remove('active')
     }
 }
+
+// Chamadas da função 
+updateValues();
